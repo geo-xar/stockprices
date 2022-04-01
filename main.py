49ff535cef9133c0f767be81ff47508b622c12e7
Georgios Charitos <geoxarg@gmail.com>
2022-04-01 08:45:58 +0100

    Updated the generated excel file name.

--- main.py
+++ main.py
@@ -6,19 +6,27 @@ import sys
 from datetime import datetime
 from StockPrices import StockPrices
 
+def add_zero_prefix_if_less_than_ten(integer):
+    if integer < 10:
+        integerStr = '0' + str(integer)
+    else:
+        integerStr = str(integer)
+    return integerStr
+
 def main():
     stockPrices = StockPrices(sys.argv)
 
-    stock_symbols = ['MTTR','AFRM','SHOP','LMND','PYPL','FB','BYND','ZM','PLTR','NFLX','COIN','HOOD','SOFI',
-                     'BABA','AMD','HNST','TSLA','SQ','ADSK','OTLY','SAM','JPM','NVDA','GS','ADBE','ABNB',
-                     'WBA','NKE','DKNG','AMZN','SBUX','TTCF','ETSY','DIS','V','MSFT','AAPL','GOOGL',
-                     'BRK-B','NET','KO','CRSR','JWN','ENPH']
+    stock_symbols = ['MTTR','AFRM','SHOP','PYPL','FB','SOFI','HNST','OTLY','NFLX','ZM','BYND','LMND','DKNG',
+                     'ETSY','PLTR','COIN','HOOD','ADSK','ADBE','SAM','BABA','WBA','SBUX','AMD','TTCF','GS',
+                     'JPM','NKE','DIS','TSLA','SQ','MSFT','CRSR','KO','AAPL','GOOGL','AMZN','V','ABNB','NVDA',
+                     'BRK-B','NET','JWN','ENPH']
 
     stocks_list = stockPrices.request_stock_prices(stock_symbols)
 
     dateTimeObj = datetime.now()
-    dateTimeStr = str(dateTimeObj.year) + 'y' + str(dateTimeObj.month) + 'm' + str(dateTimeObj.day) + 'd' + str(dateTimeObj.hour) + 'h' + str(dateTimeObj.minute) + 'm' + str(dateTimeObj.second) + 's'
-    workbook = xlsxwriter.Workbook('Stocks' + dateTimeStr + '.xlsx')
+    dateTimeStr = add_zero_prefix_if_less_than_ten(dateTimeObj.year) + '-' + add_zero_prefix_if_less_than_ten(dateTimeObj.month) + '-' + add_zero_prefix_if_less_than_ten(dateTimeObj.day)
+    dateTimeStr = dateTimeStr + '_' + add_zero_prefix_if_less_than_ten(dateTimeObj.hour) + '.' + add_zero_prefix_if_less_than_ten(dateTimeObj.minute) + '.' + add_zero_prefix_if_less_than_ten(dateTimeObj.second)
+    workbook = xlsxwriter.Workbook('Stocks_' + dateTimeStr + '.xlsx')
     worksheet = workbook.add_worksheet()
 
     for row_num, stock in enumerate(stocks_list):
