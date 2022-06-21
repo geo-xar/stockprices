3449943516a200a07b0a4367140fef39fe2e9917
ilias4780 <ilias4780@hotmail.com>
2022-06-21 11:00:25 +0100

    added selection box for charts

--- main.py
+++ main.py
@@ -18,26 +18,28 @@ def main():
                      'CRSR','TTCF','SBUX','NFLX','TTD','JWN','PLTR','ENPH']
 
     # request real time stock data
-    real_time_stocks_df = stock_prices.request_real_time_stock_prices(stock_symbols)
+    if "real_time_stocks_df" not in st.session_state:
+        st.session_state.real_time_stocks_df = stock_prices.request_real_time_stock_prices(stock_symbols)
     # output them into an excel spreadsheet (commented out)
     date_time_now_str = datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
     # workbook_name = 'Stocks_' + date_time_now_str + '.xlsx'
     # real_time_stocks_df.to_excel(workbook_name)
 
     # request historical stock data
-    stock_history_dict_of_dfs = stock_prices.request_historical_stock_prices(stock_symbols)
+    if "stock_history_dict_of_dfs" not in st.session_state:
+        st.session_state.stock_history_dict_of_dfs = stock_prices.request_historical_stock_prices(stock_symbols)
 
     # Create the dashboard
     st.set_page_config(layout="wide")
     st.title("Stock Prices Application")
 
     st.header(f"Real time stock prices retrieved at {date_time_now_str}:")
-    st.dataframe(real_time_stocks_df)
+    st.dataframe(st.session_state.real_time_stocks_df)
 
     st.header(f"Historical stock data:")
-    for company_name, stock_df in stock_history_dict_of_dfs.items():
-        st.subheader(company_name)
-        st.line_chart(stock_df)
+    st.subheader("Select company's symbol to display historical stock price chart:")
+    symbol = st.selectbox("Symbol", st.session_state.stock_history_dict_of_dfs.keys())
+    st.line_chart(st.session_state.stock_history_dict_of_dfs[symbol])
 
 
 if __name__ == '__main__':
